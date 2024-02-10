4669f0bd9a82c2c5e10381a1769b9c978ecd2e63
Erin A <erinaviles83@gmail.com>
2024-02-09 21:37:37 -0500

    moved functions to appropriate JS and CSS files

--- src/App.js
+++ src/App.js
@@ -1,72 +1,20 @@
 import React from "react";
-import "./index.css";
+import Weather from "./Weather";
+import "./App.css";  
 
-export default function Weather() {
-  let weatherData = {
-    city: "Tampa",
-    temperature: 65,
-    date: "Thursday 9:30",
-    description: "Clear",
-    imgUrl:
-      "https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png",
-    humidity: 60,
-    wind: 20,
+
+
+export default function App () {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather weatherData="Tampa" />
+  
+    <footer>
+              This project was coded by Erin Aviles and is {" "} <a href="https://github.com/erinaviles83/react-wk5-project" target="_blank"> open-sourced on GitHub ☀️</a>
+            </footer>
+    </div>
+    </div>
+  )
   };
 
-  return (
-    <div className="container">
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    </div>
-    
-  );
-}
